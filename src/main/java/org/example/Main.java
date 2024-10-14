32e295db59da0a6e9965f36fdb5ef96a82dec3a4
kmjanith <kavinduj.20@cse.mrt.ac.lk>
2024-10-14 18:17:58 +0530

    example semaphoere code added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,35 @@
 package org.example;
 
+import org.example.semphoreexample.MyThread;
+import org.example.semphoreexample.Shared;
+
+import java.util.concurrent.Semaphore;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //creating a semaphore
+        Semaphore sem = new Semaphore(1);
+
+        //creating two threads A will increment the count B will decrement the count
+        Thread A = new MyThread(sem, "A");
+        Thread B = new MyThread(sem, "B");
+
+        //starting threads
+        A.start();
+        B.start();
+
+        //waiting for threads
+        try
+        {
+            A.join();
+            B.join();
+        }catch (InterruptedException e){
+            throw new RuntimeException(e);
+        }
+
+        //count should be 0
+        System.out.println("count: " + Shared.count);
+
     }
 }
